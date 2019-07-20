f3745b6b6f95d3b2055044b5fea43bb121bf3b73
Jedrzej Ostrowski <jedrzej.ostrowski@smartfactor.pl>
2019-07-20 21:43:48 +0200

    Initial commit

---
+++ src/main/java/com/peoplepedia/PeoplepediaApplication.java
@@ -0,0 +1,13 @@
+package com.peoplepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplepediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeoplepediaApplication.class, args);
+    }
+
+}
